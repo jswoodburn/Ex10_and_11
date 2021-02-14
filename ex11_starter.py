5ac70c6eac5403ab45eb40d733df2e9b944c9ef5
Tasnim <Tasnim.b98@gmail.com>
2021-02-14 10:59:07 +0000

    exercise 11 complete

--- ex11_starter.py
+++ ex11_starter.py
@@ -2,3 +2,16 @@
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 
+print('-' * len(Belgium))
+print(Belgium.replace(',' , ':'))
+bel_list = Belgium.split(',')
+print(bel_list)
+bel_pop = (int(bel_list[1]) + int(bel_list[3]))
+print("The population of Belgium is " + str(bel_pop))
+
+s = "{:s}: {:010d}".format("The population of Belgium is",bel_pop)
+print(s)
+
+listt = "The population of Belgium is"
+s = f"{listt:s}: {bel_pop:010d}"
+print(s)
--- string_pdf.py
+++ string_pdf.py
@@ -8,14 +8,39 @@
 # print(str.rstrip())
 
 # String Formatting
-planets = {'mercury': 57.91,
-'menus': 108.2,
-'earth': 149.597870,
-'mars': 227.94
-}
-
-for i, key in enumerate(planets.keys(), 1):
-    print("{:2f} {:<10s} {:06.2f} Gm".format(i, key.capitalize(), planets[key]))
+# planets = {'mercury': 57.91,
+# 'menus': 108.2,
+# 'earth': 149.597870,
+# 'mars': 227.94
+# }
+#
+# for i, key in enumerate(planets.keys(), 1):
+#     print("{:2f} {:<10s} {:06.2f} Gm".format(i, key.capitalize(), planets[key]))
 
 # Literal string interpolation
 
+# names = ['Tom', 'Harry', 'Jane', 'Mary']
+# s = f"The third member is {names[3]}"
+# print(s)
+# for i, key in enumerate(planets.keys(), 1):
+#     print(f"{i:2d} {key.lower():<10s} {planets[key] :06.2f} Gm")
+
+
+# literal string interpolation 2
+
+# suffix = ['1st', 'nd', 'rd', 'th']
+# n = 2
+# s = f"{str(n+1) + suffix[n]} of {len(names) : >8d} : {names[n]: >10s} "
+# print(s)
+
+# STRING METHODS -SPLIT AND JOIN
+
+line = 'root::0:0:superuser:/root:bin/sh'
+elems = line.split(':')
+print(elems)
+elems[0] = 'avatar'
+elems[4] = 'The super-user (zero)'
+print(elems)
+
+line = ':'.join(elems)
+print(line)
